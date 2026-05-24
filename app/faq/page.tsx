b2ce2a39c3e99ebba3d4d403772cd1c2586d43cc
Richie <Richard.layhout.lao@gmail.com>
2026-05-24 19:22:46 -0400

    Add site pages and bump React versions
    Add many new site pages (about, contact, FAQ, pricing, testimonials, portfolio and portfolio subpages) and update the app root page with navigation, imagery, and content sections.

---
+++ app/faq/page.tsx
@@ -0,0 +1,44 @@
+export default function Page() {
+  return <div><section>
+  <h1>FAQ</h1>
+
+  <div>
+    <p>How should we dress for our family session?</p>
+    <p>
+      Similar colour palette is more pleasing to the eye and it stands the test of time.
+      Don’t worry; I will share a checklist a few days before the shoot.
+    </p>
+  </div>
+
+  <div>
+    <p>What’s the best time to have a session with kids?</p>
+    <p>
+      Mornings are best. Afternoons are great. We should take advantage of kids' high energy.
+      Evenings are generally avoided because everyone is tired.
+    </p>
+  </div>
+
+  <div>
+    <p>When is the best time to do a maternity photoshoot?</p>
+    <p>
+      The ideal time is between 5–7 months of pregnancy. At that stage, the belly is nicely
+      shaped and the mother can still move comfortably.
+    </p>
+  </div>
+
+  <div>
+    <p>How long is a shoot?</p>
+    <p>
+      On average, a photoshoot is about 1–1.5 hours. Beyond that, energy and natural flow
+      usually start to drop.
+    </p>
+  </div>
+
+  <div>
+    <p>When can we expect to receive the photos?</p>
+    <p>
+      Depending on workload, delivery is usually within 1–2 months after the photoshoot.
+    </p>
+  </div>
+</section></div>
+}
---
+++ app/portfolio/kids/page.tsx
@@ -0,0 +1,30 @@
+export default function Page() {
+  return <div>
+    <section>
+  <div>
+    <h1>Kids</h1>
+  </div>
+</section>
+
+<section>
+  <div>
+    <img src="//images-pw.pixieset.com/elementfield/pyKnnRX/W26_PC_Portfolio_ChloeColour_Expand_Quach_CleanBG-95d67773-1500.jpg" alt=""/>
+    <img src="//images-pw.pixieset.com/elementfield/pyKnnRX/W26_PC_Portfolio_KenjiColour_Quach_CleanBG-b560c226-1500.jpg" alt=""/>
+    <img src="//images-pw.pixieset.com/elementfield/pyKnnRX/W26_PC_Portfolio_AylaColour_Quach_CleanBG-f7eb4f06-1500.jpg" alt=""/>
+  </div>
+
+  <div>
+    <img src="//images-pw.pixieset.com/elementfield/pyKnnRX/W26_PC_Portfolio_JayceColour_Quach_CleanBG-055ccdf5-1500.jpg" alt=""/>
+    <img src="//images-pw.pixieset.com/elementfield/pyKnnRX/W26_PC_Portfolio_KittyColour_Quach_CleanBG-a1fc63e1-1500.jpg" alt=""/>
+    <img src="//images-pw.pixieset.com/elementfield/pyKnnRX/W26_PC_Portfolio_ThomasColor_Quach_CleanedBG-842f3eb0-1500.jpg" alt=""/>
+  </div>
+
+  <div>
+    <img src="//images-pw.pixieset.com/elementfield/pyKnnRX/W26_PC_Portfolio_LiliaColour_Expand_Quach_CleanBG-25ea78a0-1500.jpg" alt=""/>
+    <img src="//images-pw.pixieset.com/elementfield/pyKnnRX/W26_PC_Portfolio_MilanaColour2_Quach_CleanBG-d75dfa53-1500.jpg" alt=""/>
+    <img src="//images-pw.pixieset.com/elementfield/pyKnnRX/Lucas2915-7698bc43-1500.jpg" alt=""/>
+  </div>
+</section>
+
+</div>
+}
