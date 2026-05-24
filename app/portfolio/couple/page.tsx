55de754aeeadd22f30b015e62a9cea5da33109fe
Richie <Richard.layhout.lao@gmail.com>
2026-05-24 19:22:46 -0400

    Add site pages and bump React versions
    Add many new site pages (about, contact, FAQ, pricing, testimonials, portfolio and portfolio subpages) and update the app root page with navigation, imagery, and content sections.

---
+++ app/portfolio/couple/page.tsx
@@ -0,0 +1,27 @@
+export default function Page() {
+  return <div>
+    <section>
+  <div>
+    <h1>Love Birds</h1>
+  </div>
+</section>
+
+<section>
+  <div>
+    <img src="//images-pw.pixieset.com/elementfield/6GW5JyO/8E9A0743-Enhanced-NR-Edit-615309f7-1500.jpg" alt=""/>
+  </div>
+
+  <div>
+    <img src="//images-pw.pixieset.com/elementfield/6GW5JyO/8E9A0537-Enhanced-NR-Edit-23d884d7-1500.jpg" alt=""/>
+  </div>
+
+  <div>
+    <img src="//images-pw.pixieset.com/elementfield/6GW5JyO/8E9A0794-Enhanced-NR-Edit-63a7e271-1500.jpg" alt=""/>
+  </div>
+
+  <div>
+    <img src="//images-pw.pixieset.com/elementfield/6GW5JyO/8E9A0643-Enhanced-NR-Edit-91106082-1500.jpg" alt="" />
+  </div>
+</section>
+  </div>
+}
