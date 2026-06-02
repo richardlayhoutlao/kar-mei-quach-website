3677a070851d08e590742cb45f3c7e8b0be5bb20
Richie <53919513+richardlayhoutlao@users.noreply.github.com>
2026-06-02 09:29:09 -0400

    feature/pricing (#4)

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,45 +1,5 @@
-export default function Page() {
-  return <div>
-<div>
+import { PricingSection } from "@/components/layout/Pricing/PricingSection"
 
-  <div>
-    <div>
-      <h3>Couple</h3>
-      <p>Beautiful love</p>
-      <span>from: $500</span>
-    </div>
-    <img src="https://images-pw.pixieset.com/elementfield/yWxYV5n/Nidarsan-e61ea278-1000.jpg" alt="Couple - Beautiful love"/>
-  </div>
+const Page = () => <PricingSection />
 
-  <div>
-    <div>
-      <h3>Maternity</h3>
-      <p>Precious moments before the arrival of the bundle of joy</p>
-      <span>from: $500</span>
-    </div>
-    <img src="https://images-pw.pixieset.com/elementfield/0Kl51Po/VictoriaPregnancy2481-Edit-1db8cd83-1000.jpg" alt="Maternity - Precious moments"/>
-  </div>
-
-  <div>
-    <div>
-      <h3>Family</h3>
-      <p>Mamas don't only take photos. They can also be part of the photos.</p>
-      <span>from: $500</span>
-    </div>
-    <img src="https://images-pw.pixieset.com/elementfield/M4G1PDd/EmergeAyla-21-6b9c3982-1000.jpg" alt="Family - natural moments"/>
-  </div>
-
-  <div>
-    <div>
-      <h3>Kids</h3>
-      <p>Spontaneous expressions</p>
-      <span>from: $500</span>
-    </div>
-    <img src="https://images-pw.pixieset.com/elementfield/VMDLKmm/8E9A2761-Edit-ffddf968-1000.jpg" alt="Kids - spontaneous expressions"/>
-  </div>
-
-</div>
-
-
-</div>
-}
+export default Page
