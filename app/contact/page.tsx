ef69848dc92e4433c0684d3fa52ac19afb0955a3
Richie <53919513+richardlayhoutlao@users.noreply.github.com>
2026-06-02 10:19:19 -0400

    feature/contact (#5)

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,22 +1,5 @@
-export default function Page() {
-  return <div><section>
-  <form action="mailto:info@karmeiquach.com" method="post" encType="text/plain">
-    <div>
-      <label>Name *</label>
-      <input type="text" name="name"/>
-    </div>
+import { ContactSection } from "@/components/layout/Contact/ContactSection"
 
-    <div>
-      <label>Email address *</label>
-      <input type="email" name="email"/>
-    </div>
+const Page = () => <ContactSection />
 
-    <div>
-      <label>Message *</label>
-      <textarea name="message" rows={5}></textarea>
-    </div>
-
-    <button type="submit">Send Message</button>
-  </form>
-</section></div>
-}
+export default Page
