68b34094251614859a2cc0bf131b7c9d46982323
Richie <53919513+richardlayhoutlao@users.noreply.github.com>
2026-06-02 11:20:08 -0400

    Feature/testimonials (#6)
    * Add TestimonialsSection component
    
    * Add FAQ section and animated UI components

--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -1,43 +1,5 @@
-export default function Page() {
-  return <div><section>
-  <div>
-    <h2>Pierre & Abi</h2>
-    <p>
-      An incredible experience with Kar-Mei. Her talent and sensitivity truly make
-      all the difference. These are memories we’ll cherish for our entire lives.
-      Thank you so much!
-    </p>
-  </div>
+import { TestimonialsSection } from "@/components/layout/Testimonials/TestimonialsSection"
 
-  <div>
-    <h2>Lam's Family</h2>
-    <p>
-      Kar-Mei is an incredibly professional and passionate photographer. We were
-      blessed to have her take our family and maternity pictures. Our kids had an
-      amazing time during the photoshoot and were even more over the moon when they
-      saw their pictures. We highly recommend her to help immortalize your family!
-    </p>
-  </div>
+const Page = () => <TestimonialsSection />
 
-  <div>
-    <h2>Nidarsan & Anthy</h2>
-    <p>
-      Honestly, it was a really wonderful experience. The photographer made us feel
-      comfortable right from the start, and it really shows in the photos—they’re
-      natural and spontaneous. We’re extremely happy with the result; we absolutely
-      love them!
-    </p>
-  </div>
-
-  <div>
-    <h2>Nguyen's Family</h2>
-    <p>
-      We had a wonderful experience during the photoshoot. Karmei was incredibly
-      patient and remarkably gentle, which helped the children quickly feel
-      comfortable and natural in front of the camera. The entire experience was
-      enjoyable from beginning to end.
-    </p>
-  </div>
-</section>
-</div>
-}
+export default Page
